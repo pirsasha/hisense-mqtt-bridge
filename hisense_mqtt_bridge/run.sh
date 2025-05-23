9f206aa794271b3fa60dcdf7100e6ca40b3bbd90
Aleksandr <166982493+pirsasha@users.noreply.github.com>
2025-05-23 15:44:56 +0300

    Update run.sh

--- hisense_mqtt_bridge/run.sh
+++ hisense_mqtt_bridge/run.sh
@@ -4,6 +4,7 @@ touch /app/log.txt
 echo "=== STARTING mosquitto_sub ===" >> /app/log.txt
 
 mosquitto_sub \
+  -d \
   -h 192.168.2.150 -p 36669 \
   --cert /ssl/rcm_certchain_pem.cer \
   --key /ssl/rcm_pem_privkey.pkcs8 \
