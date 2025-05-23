7d8f1444ea834d9028436c3ad876bb120f1e8163
Aleksandr <166982493+pirsasha@users.noreply.github.com>
2025-05-23 15:40:42 +0300

    Add files via upload

--- hisense_mqtt_bridge/run.sh
+++ hisense_mqtt_bridge/run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 touch /app/log.txt
-python3 bridge.py >> /app/log.txt 2>&1 &
+
+# Запускаем подписку через mosquitto_sub в фоне
+mosquitto_sub \
+  -h 192.168.2.150 -p 36669 \
+  --cert /ssl/rcm_certchain_pem.cer \
+  --key /ssl/rcm_pem_privkey.pkcs8 \
+  --insecure \
+  -u hisenseservice -P multimqttservice \
+  -v -t "#" >> /app/log.txt 2>&1 &
+
+# Запускаем web-интерфейс
 python3 webserver.py
