91f739fc28d985167139495b304f0939dca4ae81
Aleksandr <166982493+pirsasha@users.noreply.github.com>
2025-05-23 15:25:36 +0300

    Update bridge.py

--- hisense_mqtt_bridge/bridge.py
+++ hisense_mqtt_bridge/bridge.py
@@ -4,6 +4,8 @@ import paho.mqtt.client as mqtt
 
 MQTT_HOST = "192.168.2.150"
 MQTT_PORT = 36669
+MQTT_USER = "hisenseservice"
+MQTT_PASS = "multimqttservice"
 
 def on_connect(client, userdata, flags, rc):
     print("✅ Connected with result code", rc)
@@ -13,13 +15,16 @@ def on_message(client, userdata, msg):
     print(f"📩 Message from {msg.topic}: {msg.payload.decode()}")
 
 client = mqtt.Client()
+client.username_pw_set(MQTT_USER, MQTT_PASS)
+
+# Используем client cert/key, но отключаем валидацию сервера
 client.tls_set(
     ca_certs=None,
     certfile="/ssl/rcm_certchain_pem.cer",
     keyfile="/ssl/rcm_pem_privkey.pkcs8",
     tls_version=ssl.PROTOCOL_TLS_CLIENT
 )
-client.tls_insecure_set(True)  # <-- обязательно ДО connect
+client.tls_insecure_set(True)
 
 client.on_connect = on_connect
 client.on_message = on_message
